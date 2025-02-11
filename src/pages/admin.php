01da5904f79a9c1ff4ce1106689b97c709c07176
Erik Mitchell <erikdmitchell@gmail.com>
2025-02-10 19:39:41 -0500

    added admin page in boomi cms menu

---
+++ src/pages/admin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Admin Page
+ *
+ * @package BCDevTools
+ * @since   0.1.0
+ * @version 0.1.0
+ */
+
+?>
+
+<div class="wrap bc-dev-tools-admin">
+    <h1>Dev Tools</h1>
+
+    <div class="bc-admin-box">
+        <h2>AI Tool Migration Tests</h2>
+
+        <ul>
+            <li><a href="/wp-admin/admin.php?page=dev-tools&action=aitool-migrate">Migrate AI Tool Data</a></li>
+            <li><a href="/wp-admin/admin.php?page=dev-tools&action=aitool-migrate-reports">Migrate AI Tool Report Data</a></li>
+            <li><a href="/wp-admin/admin.php?page=dev-tools&action=aitool-migrate-likes">Migrate AI Tool Likes Data</a></li>
+            <li><a href="/wp-admin/admin.php?page=dev-tools&action=aitool-remove-uploads">Remove AI Tool Uploads Folder</a></li>
+        </ul>
+    </div>              
+</div>
