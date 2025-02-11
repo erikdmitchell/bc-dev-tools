9beceb02093b29ef7ef8f7baedaa9a36b36c863b
Erik Mitchell <erikdmitchell@gmail.com>
2025-02-10 19:32:07 -0500

    basic setup with admin file

---
+++ src/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace erikdmitchell\bcdevtools;
+
+class Admin {
+
+    public function __construct() {
+        // add_action( 'admin_menu', array($this, 'add_menu') );
+        echo 'bc-dev-tools Admin.php';
+    }
+}
