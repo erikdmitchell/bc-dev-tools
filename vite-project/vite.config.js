0bcbdfc9e9caf90eb8678f450e5c9399f8c6d734
Erik Mitchell <erikdmitchell@gmail.com>
2025-04-30 11:28:25 -0400

    setup basic vite project

---
+++ vite-project/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: '/index.html',
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
+});
